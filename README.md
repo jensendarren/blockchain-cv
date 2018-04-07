55bdcc1f8134673f9074c38338c586d0c1faeb68
Darren Jensen <darren.jensen@gmail.com>
2018-04-07 14:14:52 +0700

    basic react js app working with header data

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Blockchain CV App
 
+## Development mode
+
+In one terminal window run `truffle develop`
+In the truffle develop console run `migrate --reset`
+In a second terminal window run `npm run start`
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
+import CVContract from '../build/contracts/CVContract.json'
 import getWeb3 from './utils/getWeb3'
 
 import './css/oswald.css'
@@ -12,7 +12,6 @@ class App extends Component {
     super(props)
 
     this.state = {
-      storageValue: 0,
       web3: null
     }
   }
@@ -24,7 +23,12 @@ class App extends Component {
     getWeb3
     .then(results => {
       this.setState({
-        web3: results.web3
+        web3: results.web3,
+        address: "",
+        title: "",
+        description: "",
+        author_name: "",
+        author_email: ""
       })
 
       // Instantiate contract once web3 provided.
@@ -44,25 +48,39 @@ class App extends Component {
      */
 
     const contract = require('truffle-contract')
-    const simpleStorage = contract(SimpleStorageContract)
-    simpleStorage.setProvider(this.state.web3.currentProvider)
+    const cvContact = contract(CVContract)
+    cvContact.setProvider(this.state.web3.currentProvider)
 
     // Declaring this for later so we can chain functions on SimpleStorage.
-    var simpleStorageInstance
+    var cvInstance
 
     // Get accounts.
     this.state.web3.eth.getAccounts((error, accounts) => {
-      simpleStorage.deployed().then((instance) => {
-        simpleStorageInstance = instance
+      cvContact.deployed().then((instance) => {
+        cvInstance = instance
 
-        // Stores a given value, 5 by default.
-        return simpleStorageInstance.set(5, {from: accounts[0]})
-      }).then((result) => {
-        // Get the value from the contract to prove it worked.
-        return simpleStorageInstance.get.call(accounts[0])
-      }).then((result) => {
-        // Update state with the result.
-        return this.setState({ storageValue: result.c[0] })
+        // Get the address from the contract
+        return cvInstance.getAddress.call()
+      }).then((address) => {
+        // Update state with the address.
+        console.log(address);
+        this.setState({ address: address })
+        return cvInstance.getTitle()
+      }).then((title) => {
+        // Update state with the address.
+        console.log(title);
+        this.setState({ title: title })
+        return cvInstance.getDescription()
+      }).then((description) => {
+        // Update state with the description.
+        console.log(description);
+        this.setState({ description: description })
+        return cvInstance.getAuthor()
+      }).then((author) => {
+        // Update state with the author.
+        console.log(author);
+        this.setState({ author_name: author[0] })
+        this.setState({ author_email: author[1] })
       })
     })
   }
@@ -79,10 +97,12 @@ class App extends Component {
             <div className="pure-u-1-1">
               <h1>Good to Go!</h1>
               <p>Your Truffle Box is installed and ready.</p>
-              <h2>Smart Contract Example</h2>
-              <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
-              <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
-              <p>The stored value is: {this.state.storageValue}</p>
+              <h2>CV Contract Example</h2>
+              <p>The address value is: {this.state.address}</p>
+              <p>The title value is: {this.state.title}</p>
+              <p>The description value is: {this.state.description}</p>
+              <p>The author name is: {this.state.author_name}</p>
+              <p>The author email is: {this.state.author_email}</p>
             </div>
           </div>
         </main>
